ec37ec3f5f898c4eddc1602369d8f73ee31b5693
Harshit Luthra <love.coolsachin@gmail.com>
2019-01-30 22:09:23 +0530

    Add Material 2.0

--- Material20/app/src/main/java/com/crappy/material20/MainActivity.kt
+++ Material20/app/src/main/java/com/crappy/material20/MainActivity.kt
@@ -2,8 +2,6 @@
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,21 +11,23 @@ import kotlinx.android.synthetic.main.activity_main.*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(bottom_app_bar)
+
     single_chip.setOnClickListener{
         it.visibility = View.INVISIBLE
     }
+        bottom_app_bar.replaceMenu(R.menu.bottom_app_bar_menu)
+        bottom_app_bar.setOnMenuItemClickListener{ MenuItem->
+            when(MenuItem.itemId){
+                R.id.action_bar_toast->  Toast.makeText(this,"Toast",Toast.LENGTH_SHORT).show()
+                R.id.action_cart ->  Toast.makeText(this@MainActivity,"Cart ",Toast.LENGTH_SHORT).show()
+                R.id.shipping -> Toast.makeText(this@MainActivity,"Shipping",Toast.LENGTH_SHORT).show()
+                R.id.customer -> Toast.makeText(this@MainActivity,"Customer",Toast.LENGTH_SHORT).show()
+
+
+            }
+            true
+        }
     }
 
-     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-         menuInflater.inflate(R.menu.bottom_app_bar_menu,menu)
-         return true
-     }
 
-     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-         when (item?.itemId){
-             R.id.action_bar_toast->  Toast.makeText(this,"Toast",Toast.LENGTH_SHORT).show()
-         }
-return true
-     }
 }
